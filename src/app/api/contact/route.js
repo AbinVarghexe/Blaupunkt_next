547ba8532c7a34d5146c36f301b1b6928dd31218
AbinVarghexe <abinvarghese092005@gmail.com>
2025-12-18 16:22:38 +0530

    feat: implement contact form submission API endpoint using Resend for email notifications.

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
-import { EnquireTemplate } from '@/components/emails/EnquireTemplate';
+import { EnquireTemplate } from '../../../../src/components/emails/EnquireTemplate';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
